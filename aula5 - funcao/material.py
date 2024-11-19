197d241a8cf5be7fc151a2f36838dcfb2513847d
Victor Santos Rohod <victorsantosrohod@gmail.com>
2024-11-19 00:22:58 -0300

    aula5

---
+++ aula5 - funcao/material.py
@@ -0,0 +1,116 @@
+"""
+Argumentos nomeados e não nomeados em funções Python
+Argumento nomeado tem nome com sinal de igual
+Argumento não nomeado recebe apenas o argumento (valor)
+"""
+
+
+def soma(x, y, z):
+    # Definição
+    print(f'{x=} y={y} {z=}', '|', 'x + y + z = ', x + y + z)
+
+
+soma(1, 2, 3)
+soma(1, y=2, z=5)
+
+print(1, 2, 3, sep='-')
+
+"""
+Valores padrão para parâmetros
+Ao definir uma função, os parâmetros podem
+ter valores padrão. Caso o valor não seja
+enviado para o parâmetro, o valor padrão será
+usado.
+Refatorar: editar o seu código.
+"""
+def soma(x, y, z=None):
+    if z is not None:
+        print(f'{x=} {y=} {z=}', x + y + z)
+    else:
+        print(f'{x=} {y=}', x + y)
+soma(1, 2)
+soma(3, 5)
+soma(100, 200)
+soma(7, 9, 0)
+soma(y=9, z=0, x=7)
+
+"""
+Escopo de funções em Python
+Escopo significa o local onde aquele código pode atingir.
+Existe o escopo global e local.
+O escopo global é o escopo onde todo o código é alcançavel.
+O escopo local é o escopo onde apenas nomes do mesmo local
+podem ser alcançados.
+"""
+
+x = 1
+
+
+def escopo():
+    global x
+    x = 10
+
+    def outra_funcao():
+        global x
+        x = 11
+        y = 2
+        print(x, y)
+
+    outra_funcao()
+    print(x)
+
+
+print(x)
+escopo()
+print(x)
+
+"""
+Retorno de valores das funções (return)
+"""
+
+
+def soma(x, y):
+    if x > 10:
+        return [10, 20]
+    return x + y
+
+
+# variavel = soma(1, 2)
+# variavel = int('1')
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+print(soma1)
+print(soma2)
+print(soma(11, 55))
+
+"""
+args - Argumentos não nomeados
+* - *args (empacotamento e desempacotamento)
+"""
+# Lembre-te de desempacotamento
+# x, y, *resto = 1, 2, 3, 4
+# print(x, y, resto)
+
+
+# def soma(x, y):
+#     return x + y
+
+def soma(*args):
+    total = 0
+    for numero in args:
+        total += numero
+    return total
+
+
+soma_1_2_3 = soma(1, 2, 3)
+# print(soma_1_2_3)
+
+soma_4_5_6 = soma(4, 5, 6)
+# print(soma_4_5_6)
+
+numeros = 1, 2, 3, 4, 5, 6, 7, 78, 10
+outra_soma = soma(*numeros)
+print(outra_soma)
+
+print(sum(numeros))
+# print(*numeros)
