8bd943ed2028a788993c093fd4ae3c6c095f7948
Victor Santos Rohod <victorsantosrohod@gmail.com>
2024-12-04 23:00:43 -0300

    aula14

---
+++ aula14 - objetos pt2/material.py
@@ -0,0 +1,103 @@
+# __dict__ e vars para atributos de instância
+class Pessoa:
+    ano_atual = 2022
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade
+
+
+dados = {'nome': 'João', 'idade': 35}
+p1 = Pessoa(**dados)
+# p1.nome = 'EITA'
+# print(p1.idade)
+# p1.__dict__['outra'] = 'coisa'
+# p1.__dict__['nome'] = 'EITA'
+# del p1.__dict__['nome']
+# print(p1.__dict__)
+# print(vars(p1))
+# print(p1.outra)
+# print(p1.nome)
+print(vars(p1))
+print(p1.nome)
+
+# ***************************************
+# Métodos de classe + factories (fábricas)
+# São métodos onde "self" será "cls", ou seja,
+# ao invés de receber a instância no primeiro
+# parâmetro, receberemos a própria classe.
+class Pessoa:
+    ano = 2023  # atributo de classe
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    @classmethod
+    def metodo_de_classe(cls):
+        print('Hey')
+
+    @classmethod
+    def criar_com_50_anos(cls, nome):
+        return cls(nome, 50)
+
+    @classmethod
+    def criar_sem_nome(cls, idade):
+        return cls('Anônima', idade)
+
+
+p1 = Pessoa('João', 34)
+p2 = Pessoa.criar_com_50_anos('Helena')
+p3 = Pessoa('Anônima', 23)
+p4 = Pessoa.criar_sem_nome(25)
+print(p2.nome, p2.idade)
+print(p3.nome, p3.idade)
+print(p4.nome, p4.idade)
+# print(Pessoa.ano)
+# Pessoa.metodo_de_classe()
+
+# ***************************************
+# Encapsulamento (modificadores de acesso: public, protected, private)
+# Python NÃO TEM modificadores de acesso
+# Mas podemos seguir as seguintes convenções
+#   (sem underline) = public
+#       pode ser usado em qualquer lugar
+# _ (um underline) = protected
+#       não DEVE ser usado fora da classe
+#       ou suas subclasses.
+# __ (dois underlines) = private
+#       "name mangling" (desfiguração de nomes) em Python
+#       _NomeClasse__nome_attr_ou_method
+#       só DEVE ser usado na classe em que foi
+#       declarado.
+from functools import partial
+
+
+class Foo:
+    def __init__(self):
+        self.public = 'isso é público'
+        self._protected = 'isso é protegido'
+        self.__exemplo = 'isso é private'
+
+    def metodo_publico(self):
+        # self._metodo_protected()
+        # print(self._protected)
+        print(self.__exemplo)
+        self.__metodo_private()
+        return 'metodo_publico'
+
+    def _metodo_protected(self):
+        print('_metodo_protected')
+        return '_metodo_protected'
+
+    def __metodo_private(self):
+        print('__metodo_private')
+        return '__metodo_private'
+
+
+f = Foo()
+# print(f.public)
+print(f.metodo_publico())
